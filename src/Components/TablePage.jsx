e17e8392fa708f5bcf658c25af7412581f4f07af
darkhacker1906 <singhabhinandan634@gmail.com>
2024-01-08 16:21:46 +0530

    Added input box and propvided its functionality

--- src/Components/TablePage.jsx
+++ src/Components/TablePage.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { TableContainer, Table, TableBody, TableRow, TableCell, Paper, Box } from '@mui/material';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import EditCalendarOutlined from '@mui/icons-material/EditCalendarOutlined';
 
-function TablePage() {
+function TablePage({ arr, deleteTodo, setTodo }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Box sx={{ m: '2%' }}>
+      <TableContainer component={Paper}>
+        <Table id='todos'>
+          <TableBody>
+            {arr.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell>
+                  <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '99%' }}>
+                    {item}
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <DeleteOutlinedIcon onClick={() => deleteTodo(index)} />
+                      <EditCalendarOutlined onClick={() => setTodo(index)} />
+                    </Box>
+                  </Box>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
 }
 
-export default TablePage
+export default TablePage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Button, TextField } from '@mui/material';
+import TablePage from '../Components/TablePage';
+import { Typography } from '@mui/material';
 
-function HomePage() {
+function Home() {
+  const [data, setData] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+  const [arr, setArr] = useState([]);
+
+  const set_table = (data) => {
+    if (data !== '') {
+      if (editIndex !== null) {
+        let updatedArr = [...arr];
+        updatedArr[editIndex] = data;
+        setArr(updatedArr);
+        setEditIndex(null);
+      } else {
+        setArr([...arr, data]);
+      }
+      setData('');
+    }
+  };
+ 
+
+  const delete_todo = (index) => {
+    let reduced_todo = [...arr];
+    reduced_todo.splice(index, 1);
+    setArr(reduced_todo);
+  };
+
+  const set_todo = (index) => {
+    setEditIndex(index);
+    setData(arr[index]);
+  };
+  const handleKeyEnter = (event) => {
+    if (event.key === 'Enter') {
+      set_table(data);
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+    <Box>
+      <Typography variant='h3' sx={{ m: '20px', textAlign: 'center',overflow:"hidden" }}>
+        ToDo App
+      </Typography>
+      <Box sx={{ display: 'flex', m: '20px' }}>
+        <TextField
+          variant='filled'
+          onChange={(e) => setData(e.target.value)}
+          fullWidth
+          label={'Enter your Todo'}
+          value={data}
+          onKeyDown={handleKeyEnter}
+        />
+        <Button
+          variant='contained'
+          sx={{ fontWeight: 'bold', fontSize: '1.5em' }}
+          onClick={() => set_table(data)}
+        >
+          +
+        </Button>
+      </Box>
+      <TablePage arr={arr} deleteTodo={delete_todo} setTodo={set_todo} />
+    </Box>
+  );
 }
 
-export default HomePage
+export default Home;
