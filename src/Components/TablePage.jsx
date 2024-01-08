aa925004fcd8b4a4a281c86b0c96910027d45a0f
darkhacker1906 <singhabhinandan634@gmail.com>
2024-01-08 18:49:46 +0530

    Resolving bugs

--- src/Components/TablePage.jsx
+++ src/Components/TablePage.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
-import { TableContainer, Table, TableBody, TableRow, TableCell, Paper, Box } from '@mui/material';
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import EditCalendarOutlined from '@mui/icons-material/EditCalendarOutlined';
+import React from "react";
+import {
+  TableContainer,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+  Box,
+  Hidden,
+} from "@mui/material";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import EditCalendarOutlined from "@mui/icons-material/EditCalendarOutlined";
 
 function TablePage({ arr, deleteTodo, setTodo }) {
   return (
-    <Box sx={{ m: '2%' }}>
+    <Box sx={{ m: "2%",wordBreak:'break-word' }}>
       <TableContainer component={Paper}>
-        <Table id='todos'>
+        <Table id="todos">
           <TableBody>
             {arr.map((item, index) => (
               <TableRow key={index}>
                 <TableCell>
-                  <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '99%' }}>
-                    {item}
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                      <DeleteOutlinedIcon onClick={() => deleteTodo(index)} />
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      width: "99%",
+                    }}
+                  >
+                    {item.text}
+                    <Box sx={{ display: "flex", alignItems: "center" }}>
+                      <DeleteOutlinedIcon onClick={() => deleteTodo(item.id)} />
                       <EditCalendarOutlined onClick={() => setTodo(index)} />
                     </Box>
                   </Box>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,67 +1,74 @@
-import React, { useState } from 'react';
-import { Box, Button, TextField } from '@mui/material';
-import TablePage from '../Components/TablePage';
-import { Typography } from '@mui/material';
+import React, { useState } from "react";
+import { Box, Button, TextField } from "@mui/material";
+import TablePage from "../Components/TablePage";
+import { Typography } from "@mui/material";
+import { v4 as uuidv4 } from "uuid";
 
 function Home() {
-  const [data, setData] = useState('');
+  const [data, setData] = useState("");
   const [editIndex, setEditIndex] = useState(null);
   const [arr, setArr] = useState([]);
+  const id = uuidv4();
 
   const set_table = (data) => {
-    if (data !== '') {
+    if (data !== "") {
       if (editIndex !== null) {
         let updatedArr = [...arr];
-        updatedArr[editIndex] = data;
+        updatedArr[editIndex] = { id: updatedArr[editIndex].id, text: data }; 
         setArr(updatedArr);
         setEditIndex(null);
       } else {
-        setArr([...arr, data]);
+        setArr([...arr, { id: id, text: data }]);
       }
-      setData('');
+      setData("");
     }
   };
- 
 
-  const delete_todo = (index) => {
-    let reduced_todo = [...arr];
-    reduced_todo.splice(index, 1);
+  const delete_todo = (id) => {
+    let reduced_todo = arr.filter((item) => item.id !== id);
     setArr(reduced_todo);
   };
 
-  const set_todo = (index) => {
-    setEditIndex(index);
-    setData(arr[index]);
+  const set_todo = (id) => {
+    setEditIndex(id);
+    setData(arr[id].text);
   };
+
   const handleKeyEnter = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       set_table(data);
     }
   };
 
   return (
-    <Box>
-      <Typography variant='h3' sx={{ m: '20px', textAlign: 'center',overflow:"hidden" }}>
+    <Box sx={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+
+    <Box sx={{width:"60%",overflowX:"hidden"}}>
+      <Typography
+        variant="h3"
+        sx={{ m: "20px", textAlign: "center" }}
+        >
         ToDo App
       </Typography>
-      <Box sx={{ display: 'flex', m: '20px' }}>
+      <Box sx={{ display: "flex", m: "20px" }}>
         <TextField
-          variant='filled'
+          variant="filled"
           onChange={(e) => setData(e.target.value)}
           fullWidth
-          label={'Enter your Todo'}
+          label={"Enter your Todo"}
           value={data}
           onKeyDown={handleKeyEnter}
-        />
+          />
         <Button
-          variant='contained'
-          sx={{ fontWeight: 'bold', fontSize: '1.5em' }}
+          variant="contained"
+          sx={{ fontWeight: "bold", fontSize: "1em" }}
           onClick={() => set_table(data)}
-        >
-          +
+          >
+         Submit
         </Button>
       </Box>
       <TablePage arr={arr} deleteTodo={delete_todo} setTodo={set_todo} />
+            </Box>
     </Box>
   );
 }
