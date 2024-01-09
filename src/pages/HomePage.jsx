819eb54dfce16473b74fcebd8933835ff8e3cbca
darkhacker1906 <singhabhinandan634@gmail.com>
2024-01-09 19:06:04 +0530

    Resolving  the update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,38 +2,38 @@ import React, { useState } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import TablePage from "../Components/TablePage";
 import { Typography } from "@mui/material";
-import { v4 as uuidv4 } from "uuid";
+import { useId } from "react";
 
 function Home() {
   const [data, setData] = useState("");
   const [editIndex, setEditIndex] = useState(null);
   const [arr, setArr] = useState([]);
-  const id = uuidv4();
+  const id = useId();
 
   const set_table = (data) => {
     if (data !== "") {
       if (editIndex !== null) {
         let updatedArr = [...arr];
-        updatedArr[editIndex] = { id: updatedArr[editIndex].id, text: data }; 
+        updatedArr[editIndex] = data;
         setArr(updatedArr);
         setEditIndex(null);
       } else {
-        setArr([...arr, { id: id, text: data }]);
+        setArr([...arr, data]);
       }
       setData("");
     }
   };
 
   const delete_todo = (id) => {
-    let reduced_todo = arr.filter((item) => item.id !== id);
+    let reduced_todo = [...arr];
+    reduced_todo.splice(id, 1);
     setArr(reduced_todo);
   };
 
   const set_todo = (id) => {
     setEditIndex(id);
-    setData(arr[id].text);
+    setData(arr[id]);
   };
-
   const handleKeyEnter = (event) => {
     if (event.key === "Enter") {
       set_table(data);
@@ -41,34 +41,61 @@ function Home() {
   };
 
   return (
-    <Box sx={{display:"flex",alignItems:"center",justifyContent:"center"}}>
-
-    <Box sx={{width:"60%",overflowX:"hidden"}}>
-      <Typography
-        variant="h3"
-        sx={{ m: "20px", textAlign: "center" }}
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ width: "60%" }}>
+        <Typography
+          variant="h3"
+          sx={{ m: "20px", textAlign: "center", overflow: "hidden" }}
         >
-        ToDo App
-      </Typography>
-      <Box sx={{ display: "flex", m: "20px" }}>
-        <TextField
-          variant="filled"
-          onChange={(e) => setData(e.target.value)}
-          fullWidth
-          label={"Enter your Todo"}
-          value={data}
-          onKeyDown={handleKeyEnter}
+          ToDo App
+        </Typography>
+        <Box sx={{ display: "flex", m: "20px" }}>
+          <TextField
+            id={id}
+            variant="filled"
+            onChange={(e) => setData(e.target.value)}
+            fullWidth
+            label={"Enter your Todo"}
+            value={data}
+            onKeyDown={handleKeyEnter}
           />
-        <Button
-          variant="contained"
-          sx={{ fontWeight: "bold", fontSize: "1em" }}
-          onClick={() => set_table(data)}
-          >
-         Submit
-        </Button>
+          {editIndex !== null ? (
+            <>
+              <Button
+                variant="contained"
+                sx={{ fontWeight: "bold", fontSize: ".8em" }}
+                onClick={() => set_table(data)}
+              >
+                Update
+              </Button>
+              <Button
+                variant="contained"
+                sx={{
+                  fontWeight: "bold",
+                  fontSize: ".8em",
+                  marginLeft: "10px",
+                }}
+                onClick={() => {
+                  setEditIndex(null);
+                  setData("");
+                }}
+              >
+                Cancel
+              </Button>
+            </>
+          ) : (
+            <Button
+              variant="contained"
+              sx={{ fontWeight: "bold", fontSize: "1em" }}
+              onClick={() => set_table(data)}
+            >
+              Submit
+            </Button>
+          )}
+        </Box>
+
+        <TablePage arr={arr} deleteTodo={delete_todo} setTodo={set_todo} />
       </Box>
-      <TablePage arr={arr} deleteTodo={delete_todo} setTodo={set_todo} />
-            </Box>
     </Box>
   );
 }
